248bf88d306be9a0572349f7900fc77b2cfef706
Ellen Wyllie <ellen.wyllie@gmail.com>
2020-10-21 14:59:30 -0500

    Move generated docs to new repo

---
+++ build/site/_/js/08-links.js
@@ -0,0 +1,2 @@
+$(window).on("load",function(){$.ready.then(function(){$("a").each(function(){this.hostname!==location.hostname&&(this.target="_blank",this.setAttribute("rel","noopener noreferrer"))})})});
+//# sourceMappingURL=08-links.js.map
